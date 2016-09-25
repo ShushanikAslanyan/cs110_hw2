45bfa435530cd9c9e2093138dff687a2f6f38343
ShushanikAslanyan <sh.ledzeppelin@mail.ru>
2016-09-25 18:54:51 +0400

    Update factorial.js

--- factorial.js
+++ factorial.js
@@ -1,10 +1,11 @@
 <script>
-const factorial = function(n) {
+  const factorial = function(n) {
     if(n === 0) {
-        return 1
-    } 
+        return 1;
+    }
 	{
         return n * factorial(n - 1);
-    }
-}
+    };
+};
+  console.log (factorial (5));
 </script>
