f7061af87a520d7088d4871b233e2c929c9adb55
ShushanikAslanyan <sh.ledzeppelin@mail.ru>
2016-09-26 21:00:51 +0400

    Create stars.js

---
+++ stars.js
@@ -0,0 +1,28 @@
+<script>
+ const stars = function(n) {
+  if (n === 1) {
+      return "*";
+  }
+  return "*" + stars(n-1);
+};
+
+const space = function(m) {
+  if (m === 0) {
+      return "";
+  }
+  return " " + space(m-1);
+};
+  
+    
+  const connect= function (k, spcount, stcount) {
+    if (k === 0) {
+      return "";
+       }
+    console.log(space(spcount)+ stars(stcount)) ; 
+    connect((k-1), (spcount -1), (stcount - 1));
+  };
+
+  const triangleStars = function(k) {
+    connect(k, k -1, 1)
+  };
+  </script>
